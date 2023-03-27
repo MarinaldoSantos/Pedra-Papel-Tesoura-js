d3dc630f7d20953396144c8cd13af8309f1922ad
MarinaldoSantos <126521249+MarinaldoSantos@users.noreply.github.com>
2023-03-27 12:52:32 -0300

    inserindo arquivos

---
+++ index.js
@@ -0,0 +1,47 @@
+const playText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = checkWinner();
+})); 
+
+
+function computerTurn(){
+    const randNum = Math.floor(Math.random() * 3) + 1; 
+
+    switch (randNum){
+        case 1: 
+        computer = "PEDRA";
+        break;
+        case 2: 
+        computer = "PAPEL";
+        break;
+        case 3: 
+        computer = "TESOURA";
+        break;
+    
+    }
+}
+
+function checkWinner (){
+    if(player == computer){
+        return "Empatou! ";
+    } else if (computer == "PEDRA"){
+        return (player == "PEPEL") ? "Você Ganhou!" : "Você Perdeu!";
+    }else if (computer == "PAPEL"){
+        return (player == "TESOURA") ? "Você Ganhou!" : "Você Perdeu!";
+    }else if (computer == "TESOURA"){
+        return (player == "PEDRA") ? "Você Ganhou!" : "Você Perdeu!";
+    }
+}
